39e7b3dc42042bbf14f3c81e0a5f5ddeb2bd2377
Sanem <75096823+Aisanem@users.noreply.github.com>
2021-01-22 07:58:47 +0300

    Add files via upload

---
+++ Новости/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from news.models import Category
+from news.models import Author
+from news.models import Post
+from news.models.import PostCategory
+from news. models.import Comment
+
+admin.site.register(Category)
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(PostCategory)
+admin.site.register(Comment)
+
